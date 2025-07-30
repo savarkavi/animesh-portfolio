0b97cabbf2d2d49e30bd81b73f898f5feff1f65d
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-07-30 10:25:28 +0530

    Add experienceData constant for showcasing professional experience and projects

---
+++ src/utils/constants.ts
@@ -0,0 +1,29 @@
+export const experienceData = [
+  {
+    type: "Fulltime",
+    title: "Gully Gang",
+    description:
+      "Working as a visual and digital artist handling video editing, animation, and creative direction for music releases and promotional content.",
+    time: "2024 - present",
+    imgOneSrc: "/gully-gang-img1.webp",
+    imgTwoSrc: "/gully-gang-img2.webp",
+  },
+  {
+    type: "Freelance",
+    title: "Video visualizers and concept arts",
+    description:
+      "Created animated visualizers and conceptual artwork for Sony India, filtr and other brands and independent artists.",
+    time: "2021 - 2024",
+    imgOneSrc: "/video-visualizer-img1.jpg",
+    imgTwoSrc: "/video-visualizer-img2.webp",
+  },
+  {
+    type: "Freelance",
+    title: "Video editing and mixing",
+    description:
+      "Provided post-production services including video editing, color grading, and sound mixing for music videos and promotional content.",
+    time: "2022 - 2024",
+    imgOneSrc: "/video-editing-img1.webp",
+    imgTwoSrc: "/video-editing-img2.heic",
+  },
+];
