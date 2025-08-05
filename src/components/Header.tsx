506cd8c7fd431aa0086feab6a2229726f0d0a9c6
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-08-05 09:23:54 +0530

    Refactor Header component: move headerItems definition to constants for improved maintainability

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,9 @@
 import { electroharmonix, nuku } from "@/app/page";
+import { headerItems } from "@/utils/constants";
 import Image from "next/image";
 import React from "react";
 
 const Header = () => {
-  const headerItems = [
-    { id: 1, label: "Home" },
-    { id: 2, label: "Works" },
-    { id: 3, label: "About" },
-    { id: 4, label: "Contact" },
-  ];
-
   return (
     <div className="absolute top-0 z-10 hidden w-full justify-between p-6 md:flex">
       <div className="flex w-full items-center justify-center gap-10 xl:justify-start">
