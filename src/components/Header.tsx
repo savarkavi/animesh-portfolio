544088875731e0afb2b8f1f3cc52531b1f6cd8d8
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-08-10 06:36:58 +0530

    Refactor Header component: integrate GSAP animations for button interactions and update button class for animation effects

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,28 @@
+"use client";
+
 import { electroharmonix, nuku } from "@/app/page";
 import { headerItems } from "@/utils/constants";
-import Image from "next/image";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 import React from "react";
 
+gsap.registerPlugin(useGSAP);
+
 const Header = () => {
+  useGSAP(() => {
+    gsap
+      .timeline()
+      .to(".header-btn", { y: 30, stagger: 0.1 })
+      .to(".header-btn", { y: 0, stagger: 0.1 }, "-=0.3");
+  });
+
   return (
     <div className="absolute top-0 z-10 hidden w-full justify-between p-6 md:flex">
       <div className="flex w-full items-center justify-center gap-10 xl:justify-start">
         {headerItems.map((item) => (
           <button
             key={item.id}
-            className="cursor-pointer bg-white px-4 py-1 text-sm font-extrabold text-black uppercase shadow-[-5px_5px_0px_0px_#6D28D9]"
+            className="header-btn -translate-y-50 cursor-pointer bg-white px-4 py-1 text-sm font-extrabold text-black uppercase shadow-[-5px_5px_0px_0px_#6D28D9]"
           >
             {item.label}
           </button>
