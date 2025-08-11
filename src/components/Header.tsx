5ad9211cc2e663fa5717004df067d68c5117548d
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-08-11 16:06:51 +0530

    update styling of header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { electroharmonix, nuku } from "@/app/page";
 import { headerItems } from "@/utils/constants";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import Link from "next/link";
 import React from "react";
 
 gsap.registerPlugin(useGSAP);
@@ -17,14 +17,14 @@ const Header = () => {
   });
 
   return (
-    <div className="absolute top-0 z-10 hidden w-full justify-between p-6 md:flex">
+    <div className="absolute top-0 z-10 hidden h-[80px] w-full justify-between p-6 md:flex">
       <div className="flex w-full items-center justify-center gap-10 xl:justify-start">
         {headerItems.map((item) => (
           <button
             key={item.id}
-            className="header-btn -translate-y-50 cursor-pointer bg-white px-4 py-1 text-sm font-extrabold text-black uppercase shadow-[-5px_5px_0px_0px_#6D28D9]"
+            className="header-btn -translate-y-50 cursor-pointer border bg-white px-4 py-1 text-sm font-extrabold text-black uppercase shadow-[-5px_5px_0px_0px_#6D28D9]"
           >
-            {item.label}
+            <Link href={item.link}>{item.label}</Link>
           </button>
         ))}
       </div>
