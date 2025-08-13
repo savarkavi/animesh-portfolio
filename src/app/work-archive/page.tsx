7df82b781a996fe0652bdf95b1ce9077e9e8c1b7
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-08-13 08:35:52 +0530

    Add Work Archive Page and Title Component: Implement GSAP animations for hover effects and scrolling text

---
+++ src/app/work-archive/page.tsx
@@ -0,0 +1,102 @@
+"use client";
+
+import PreloadImage from "@/components/PreloadImage";
+import PageTitle from "@/components/work-archive/PageTitle";
+import { workCategoriesData } from "@/utils/constants";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import Link from "next/link";
+import { useRef } from "react";
+import { FaExternalLinkAlt } from "react-icons/fa";
+
+gsap.registerPlugin(useGSAP);
+
+const Page = () => {
+  const workCategoryContainerRef = useRef<HTMLDivElement>(null);
+  const hoverTls = useRef<Map<number, gsap.core.Timeline>>(new Map());
+
+  const { contextSafe } = useGSAP(() => {}, {
+    scope: workCategoryContainerRef,
+  });
+
+  const handleItemHover = contextSafe((i: number) => {
+    if (!hoverTls.current.has(i)) {
+      hoverTls.current.set(
+        i,
+        gsap
+          .timeline()
+          .to(`.work-category-${i}`, {
+            yPercent: -100,
+            duration: 0.3,
+          })
+          .to(
+            `.category-img-${i}`,
+            {
+              clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+              duration: 0.3,
+            },
+            "<",
+          ),
+      );
+    } else {
+      hoverTls.current.get(i)?.play();
+    }
+  });
+
+  const handleItemLeave = contextSafe((i: number) => {
+    hoverTls.current.get(i)?.reverse();
+  });
+
+  return (
+    <div
+      ref={workCategoryContainerRef}
+      className="relative flex min-h-screen flex-col"
+    >
+      <PageTitle />
+      <div className="flex w-full flex-col self-end bg-[#fff6db] p-6 pt-30">
+        {workCategoriesData.map((item, i) => (
+          <div
+            key={item.label}
+            className={`relative h-[100px] cursor-pointer overflow-y-hidden border bg-white text-2xl font-bold text-black uppercase shadow-[-10px_10px_0px_0px_#6D28D9] md:text-5xl`}
+            onMouseEnter={() => handleItemHover(i)}
+            onMouseLeave={() => handleItemLeave(i)}
+          >
+            <Link
+              href={`work-archive/${item.label.split(" ").join("-").toLowerCase()}`}
+              className={`work-category-${i} block h-full w-full`}
+            >
+              <div
+                className={`work-category-top-${i} flex h-full w-full items-center justify-between p-4`}
+              >
+                <p>{item.label}</p>
+                <FaExternalLinkAlt className="mr-6 text-2xl" />
+              </div>
+              <div
+                className={`work-category-bottom-${i} flex h-full w-full items-center justify-between bg-blue-400 p-4 text-white`}
+              >
+                <p>{item.label}</p>
+                <FaExternalLinkAlt className="mr-6 text-2xl" />
+              </div>
+            </Link>
+            <div
+              className={`category-img-${i} fixed right-4 bottom-4 z-[99] h-[200px] w-[150px] border lg:h-[250px] lg:w-[200px] xl:h-[450px] xl:w-[450px]`}
+              style={{
+                clipPath: "polygon(50% 50%, 50% 50%, 50% 50%, 50% 50%)",
+              }}
+            >
+              <PreloadImage
+                src={item.img}
+                alt="category image"
+                fill
+                className="object-cover"
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="h-screen bg-transparent"></div>
+    </div>
+  );
+};
+
+export default Page;
---
+++ src/components/work-archive/PageTitle.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import React, { useRef, useState } from "react";
+
+gsap.registerPlugin(useGSAP, ScrollTrigger);
+
+const PageTitle = () => {
+  const workArchiveTitleRef = useRef<HTMLDivElement>(null);
+
+  useGSAP(() => {
+    gsap.to(".archive-text", {
+      keyframes: {
+        color: [
+          "oklch(63.7% 0.237 25.331)",
+          "oklch(70% 0.2 200)",
+          "oklch(80% 0.25 120)",
+          "oklch(60% 0.3 300)",
+          "oklch(75% 0.2 40)",
+          "oklch(85% 0.15 360)",
+          "oklch(65% 0.25 150)",
+        ],
+        ease: "none",
+      },
+      duration: 3,
+      ease: "none",
+      repeatDelay: 0.2,
+      repeat: -1,
+      yoyo: true,
+    });
+
+    const marqueeTitles = gsap.utils.toArray(".work-title-marquee");
+
+    marqueeTitles.forEach((_, i) => {
+      gsap.to(`.work-title-marquee-${i}`, {
+        xPercent: i % 2 === 0 ? -50 : 0,
+        ease: "none",
+        scrollTrigger: {
+          trigger: workArchiveTitleRef.current,
+          start: "top top",
+          end: "+=3000",
+          scrub: 1,
+        },
+      });
+    });
+  });
+
+  return (
+    <div
+      ref={workArchiveTitleRef}
+      className="relative flex h-screen w-full items-end overflow-hidden bg-[#fff6db]"
+    >
+      <div
+        className={`absolute mt-32 flex h-[calc(100vh-80px)] w-full flex-col justify-start gap-8 overflow-hidden text-center font-bold text-blue-400 uppercase xl:mt-0 xl:justify-end xl:gap-10`}
+      >
+        {Array.from({ length: 7 }, (_, i) => (
+          <div
+            key={i}
+            className={`work-title-marquee work-title-marquee-${i} ${i % 2 !== 0 && "-translate-x-[50%]"} mb-6 flex w-max text-5xl tracking-widest text-nowrap opacity-30 lg:text-[4rem] xl:text-[5rem] 2xl:text-[7.5rem]`}
+          >
+            <div className={"w-full md:w-screen"}>
+              <p className="outline-text scale-y-150 text-transparent">
+                Welcome to Animesh's
+              </p>
+            </div>
+            <div className={"w-full md:w-screen"}>
+              <p className="outline-text scale-y-150 text-transparent">
+                Welcome to Animesh's
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <p className="archive-text absolute top-1/2 right-1/2 mb-20 translate-x-1/2 -translate-y-1/2 scale-y-250 text-6xl uppercase lg:text-7xl 2xl:top-auto 2xl:right-8 2xl:bottom-8 2xl:translate-0 2xl:text-[11rem]">
+        archive
+      </p>
+    </div>
+  );
+};
+
+export default PageTitle;
