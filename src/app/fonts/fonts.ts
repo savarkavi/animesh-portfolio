2dd3b05ac7d8be92a84504e863ec91ae9211cf27
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-08-13 10:02:04 +0530

    updated fonts src

--- src/app/fonts/fonts.ts
+++ src/app/fonts/fonts.ts
@@ -1,11 +1,11 @@
 import localFont from "next/font/local";
 
 export const electroharmonix = localFont({
-  src: "../app/fonts/Electroharmonix.otf",
+  src: "./fonts/Electroharmonix.otf",
   variable: "--font-electroharmonix",
 });
 
 export const nuku = localFont({
-  src: "../app/fonts/nuku1.ttf",
+  src: "./nuku1.ttf",
   variable: "--font-nuku",
 });
