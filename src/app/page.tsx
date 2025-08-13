a105b0ad5b009f972f02dbd2014feca0b9a15f5a
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-08-13 09:46:43 +0530

    moved font exports to layout.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,8 @@
-import localFont from "next/font/local";
 import Hero from "@/components/Hero";
 import FeaturedWorkSection from "@/components/featured-work-section/FeaturedWorkSection";
 import ExperienceContainer from "@/components/experience/ExperienceContainer";
 
-export const electroharmonix = localFont({
-  src: "../app/fonts/Electroharmonix.otf",
-});
-
-export const nuku = localFont({
-  src: "../app/fonts/nuku1.ttf",
-});
-
-const Home = () => {
+export default function Home() {
   return (
     <div className="z-10 min-h-screen overflow-hidden">
       <Hero />
@@ -20,6 +11,4 @@ const Home = () => {
       <div className="h-screen"></div>
     </div>
   );
-};
-
-export default Home;
+}
