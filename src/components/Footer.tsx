2c519f56d152e5fee994a2fb58bcd4b8835ef8f8
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-11-09 16:18:22 +0530

    refactor Footer component layout and improve styling for better responsiveness

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -69,70 +69,71 @@ const Footer = () => {
   }, []); // Empty dependency array ensures this effect runs only once on mount
 
   return (
-    <div className="fixed top-0 left-0 z-[90] flex h-screen w-full items-center justify-center bg-black bg-[linear-gradient(135deg,_rgba(70,69,73,0.7),_rgba(0,0,0,1)_64%)] p-2 text-white xl:justify-between">
-      <div className="relative mx-auto">
-        <div className="relative flex max-w-fit flex-1 flex-col items-center">
-          <div className="flex h-full w-full flex-col items-center justify-between lg:flex-row">
-            <p
-              className={`${adeDisplay.className} text-center text-7xl uppercase md:text-8xl xl:text-[12rem]`}
-            >
-              Animesh
-            </p>
-            {/* Added a transform-style property to the parent for better 3D rendering */}
-            <div
-              ref={imageRef}
-              className="relative -mt-2 h-[100px] w-[100px] lg:h-[200px] lg:w-full"
-              style={{ transformStyle: "preserve-3d" }}
-            >
-              <Image
-                src="/footer-logo.png"
-                alt="logo"
-                fill
-                className="object-cover"
-              />
-            </div>
-          </div>
-
-          <div className="flex w-full items-center justify-between gap-4">
-            {"Thank you for visiting".split(" ").map((item, i) => (
+    <div className="bg-[linear-gradient(135deg,_rgba(213, 255, 255, 1),_rgb(59, 130, 246)_64%)] fixed top-0 left-0 z-[90] flex h-screen w-full items-center justify-center bg-[#3b82f6] p-2 text-white xl:justify-between">
+      <div className="relative mx-auto flex h-full w-full max-w-[1240px] flex-col justify-center">
+        <div>
+          <div className="relative flex max-w-fit flex-1 flex-col items-center">
+            <div className="flex h-full w-full flex-col items-center justify-between lg:flex-row">
               <p
-                key={i}
-                className="text-xl leading-10 uppercase md:text-2xl xl:text-8xl"
+                className={`${adeDisplay.className} text-center text-7xl uppercase md:text-8xl xl:text-[12rem]`}
               >
-                {item}
+                Animesh
               </p>
-            ))}
-            <div className="heart-svg relative h-[30px] w-[30px] md:h-[60px] md:w-[60px]">
-              <Image
-                src="/heart-svgrepo-com (2).svg"
-                alt="heart svg"
-                fill
-                className="object-cover"
-              />
+              <div
+                ref={imageRef}
+                className="relative -mt-2 h-[100px] w-[100px] lg:h-[200px] lg:w-full"
+                style={{ transformStyle: "preserve-3d" }}
+              >
+                <Image
+                  src="/footer-logo.png"
+                  alt="logo"
+                  fill
+                  className="object-cover"
+                />
+              </div>
+            </div>
+
+            <div className="flex w-full items-center justify-between gap-4">
+              {"Thank you for visiting".split(" ").map((item, i) => (
+                <p
+                  key={i}
+                  className="text-xl leading-10 uppercase md:text-2xl xl:text-8xl"
+                >
+                  {item}
+                </p>
+              ))}
+              <div className="heart-svg relative h-[30px] w-[30px] md:h-[60px] md:w-[60px]">
+                <Image
+                  src="/heart-svgrepo-com (2).svg"
+                  alt="heart svg"
+                  fill
+                  className="object-cover"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="mt-12 flex w-full justify-between gap-8 text-sm uppercase xl:mt-16">
+            <div className="flex flex-col items-center gap-8 text-[12px] tracking-wide">
+              {socialLinks.map((item, i) => (
+                <LinkHoverUnderline
+                  href={item.link}
+                  key={i}
+                  className="w-fit md:text-base"
+                >
+                  {item.name}
+                </LinkHoverUnderline>
+              ))}
+            </div>
+            <div className="flex flex-col items-center gap-8 text-[12px] tracking-wide">
+              <p className="md:text-base">Mail: animesh_sesnsie@gmail.com</p>
+              <p className="md:text-base">Phone: 9840771756</p>
             </div>
           </div>
         </div>
-        <div className="mt-12 flex w-full justify-between gap-8 text-sm uppercase xl:mt-16">
-          <div className="flex flex-col items-center gap-8 text-[12px] tracking-wide">
-            {socialLinks.map((item, i) => (
-              <LinkHoverUnderline
-                href={item.link}
-                key={i}
-                className="w-fit md:text-base"
-              >
-                {item.name}
-              </LinkHoverUnderline>
-            ))}
-          </div>
-          <div className="flex flex-col items-center gap-8 text-[12px] tracking-wide">
-            <p className="md:text-base">Mail: animesh_sesnsie@gmail.com</p>
-            <p className="md:text-base">Phone: 9840771756</p>
-          </div>
-        </div>
+        <p className="absolute bottom-4 z-10 uppercase">
+          © Copyright Animesh 2025
+        </p>
       </div>
-      <p className="absolute bottom-4 left-4 z-10 uppercase 2xl:left-12">
-        © Copyright Animesh
-      </p>
     </div>
   );
 };
